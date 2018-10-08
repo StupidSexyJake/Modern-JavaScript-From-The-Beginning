eb6b7148b2d9a9c643cb79b6967c8759c4b09ba2
Jake <jake@goldcoastmaids.com.au>
2018-10-08 17:11:47 +1000

    feat: Complete scope  exercises

--- 002 - JavaScript Language Fundamentals/js_sandbox/app.js
+++ 002 - JavaScript Language Fundamentals/js_sandbox/app.js
@@ -763,3 +763,38 @@ val = window.navigator.vendor;
 val = window.navigator.language;
 
 console.log(val); */
+
+
+
+
+// :::::::::::::::
+// Scope
+// :::::::::::::::
+
+// Global scope
+var a = 1;
+let b = 2;
+const c = 3;
+
+function test() {
+    // Function scope
+    var a = 4;
+    let b = 5;
+    const c = 6;    
+    console.log("Function scope ", a, b, c);
+}
+test();
+
+if(true) {
+    // Block scope
+    var a = 4;
+    let b = 5;
+    const c = 6;
+    console.log("Block scope: ", a, b, c);
+}
+
+for(var a = 0; a < 10; a++){
+    console.log(`Loop: ${a}`);
+}
+
+console.log("Global scope ", a, b, c);
